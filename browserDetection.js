bc457337340ede7d1fc4201b8937930e0be93de7
Anna Ricci Benson <angels.ricci5@gmail.com>
2014-03-31 19:40:10 +0530

    Removed unwanted toLowercase function

--- browserDetection.js
+++ browserDetection.js
@@ -1,27 +1,27 @@
 function browserDetection () {
     var userAgent = navigator.userAgent.toString().toLowerCase();
-    if (userAgent.toLowerCase().indexOf('windows') !== -1) //windows OS
+    if (userAgent.indexOf('windows') !== -1) //windows OS
         $('body').addClass('windows');
-    if (userAgent.toLowerCase().indexOf('linux') !== -1) //linux OS
+    if (userAgent.indexOf('linux') !== -1) //linux OS
         $('body').addClass('linux');
-    if (userAgent.toLowerCase().indexOf('macintosh') !== -1) //mac OS
+    if (userAgent.indexOf('macintosh') !== -1) //mac OS
         $('body').addClass('mac');   
-    if ((userAgent.toLowerCase().indexOf('ipad') !== -1) || (userAgent.toLowerCase().indexOf('mobile') !== -1) || (userAgent.toLowerCase().indexOf('android') !== -1) || (userAgent.toLowerCase().indexOf('blackberry') !== -1)) //mobile device 
+    if ((userAgent.indexOf('ipad') !== -1) || (userAgent.indexOf('mobile') !== -1) || (userAgent.toLowerCase().indexOf('android') !== -1) || (userAgent.toLowerCase().indexOf('blackberry') !== -1)) //mobile device 
         $('body').addClass('mobile');
-    if (userAgent.toLowerCase().indexOf('ipad') !== -1)
+    if (userAgent.indexOf('ipad') !== -1)
         $('body').addClass('ipad');
-    if (userAgent.toLowerCase().indexOf('blackberry') !== -1)
+    if (userAgent.indexOf('blackberry') !== -1)
         $('body').addClass('blackberry');
-    if (userAgent.toLowerCase().indexOf('android') !== -1)
+    if (userAgent.indexOf('android') !== -1)
         $('body').addClass('android');
-    if (userAgent.toLowerCase().indexOf('msie') !== -1) { //IE expect IE11
+    if (userAgent.indexOf('msie') !== -1) { //IE expect IE11
         $('body').addClass('ie');
-        if (userAgent.toLowerCase().indexOf('msie 8') !== -1) //IE8
+        if (userAgent.indexOf('msie 8') !== -1) //IE8
             $('body').addClass('ie8');
     }
-    else if (userAgent.toLowerCase().indexOf('firefox') !== -1) //Firefox
+    else if (userAgent.indexOf('firefox') !== -1) //Firefox
         $('body').addClass('firefox');
-    else if (userAgent.toLowerCase().indexOf('opera') !== -1) //Opera
+    else if (userAgent.indexOf('opera') !== -1) //Opera
         $('body').addClass('opera');
     else if ((userAgent.indexOf('safari') != -1) && !(userAgent.indexOf('chrome') != -1)) //Safari
         $('body').addClass('safari');
